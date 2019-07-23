0408ce1f3aa9fbcd3510ed07b082c5db8fedd4d9
Emerson Ford <emersontford@gmail.com>
2019-07-23 00:11:11 -0700

    Adding a few more hosts to update-all.sh

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-hosts=(home rpi ford1 ford2 ford3 miyako notchpeak.chpc.utah.edu cade hacktheu-main)
+hosts=(home rpi ford1 ford2 ford3 miyako notchpeak.chpc.utah.edu cade hacktheu-main hacktheu-quill hacktheu-bigboi)
 
 for i in ${hosts[@]}; do
-  ssh $i 'bash -s' < ~/.dotfiles/bin/dotfile-install
+  echo "updating $i"
+  ssh -o ConnectTimeout=5 $i 'bash -s' < ~/.dotfiles/bin/dotfile-install
 done

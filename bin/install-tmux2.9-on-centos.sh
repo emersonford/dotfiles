64e4bea054a5867060493bea16de9e5aa6143e4c
Emerson Ford <emersontford@gmail.com>
2019-08-14 16:04:09 -0600

    Adding libevent things for tmux installation

--- bin/install-tmux2.9-on-centos.sh
+++ bin/install-tmux2.9-on-centos.sh
@@ -1,4 +1,9 @@
-yum install libevent-devel ncurses-devel glibc-static
+# yum install libevent-devel ncurses-devel glibc-static
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+tar xzvf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure && make
+sudo make install
 
 wget https://github.com/tmux/tmux/releases/download/2.9/tmux-2.9.tar.gz
 tar xzf tmux-2.9.tar.gz

d5bb567640fa59d33d7f0381cdbb9ee8d0302335
Emerson Ford <emersontford@gmail.com>
2019-08-14 15:11:32 -0600

    Moving install-vim8-on-centos to build from packages to build from source.

--- bin/install-vim8-on-centos.sh
+++ bin/install-vim8-on-centos.sh
@@ -1,6 +1,8 @@
-yum-config-manager --add https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo
-# Remove vim-minimal if is installed
-yum remove -y vim-minimal
-yum install vim
-# If you removed vim-minimal, sudo may be removed to, install it again
-yum install sudo
+yum -y groupinstall "Development Tools"
+yum -y install ncurses-devel git-core
+
+git clone https://github.com/vim/vim && cd vim
+
+./configure --prefix=/usr --with-features=huge --enable-multibyte --with-python-config-dir=/usr/lib/python2.7/config --enable-pythoninterp=yes
+
+make && sudo make install

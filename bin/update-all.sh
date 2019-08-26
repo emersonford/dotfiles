243a22d2fdfac344d2d7113492fe945fca270e1b
Emerson Ford <emersontford@gmail.com>
2019-08-25 21:28:46 -0600

    Adding ultisnips and removing some host entries.

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hosts=(home ford1 ford2 ford3 miyako notchpeak.chpc.utah.edu cade hacktheu-main hacktheu-bigboi)
+hosts=(home fm-test miyako notchpeak.chpc.utah.edu cade hacktheu-main hacktheu-bigboi)
 
 for i in ${hosts[@]}; do
   echo "updating $i"

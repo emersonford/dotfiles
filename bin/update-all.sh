96a7cbfb6edae14b6f3af6ce68c02852b1b2315a
Emerson Ford <emersontford@gmail.com>
2019-10-16 19:24:37 -0600

    Adding/removing hostnames

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hosts=(home fm-test miyako notchpeak.chpc.utah.edu cade hacktheu-main hacktheu-bigboi)
+hosts=(home fm-test miyako notchpeak.chpc.utah.edu cade hacktheu-main hacktheu-bigboi hacktheu-development)
 
 for i in ${hosts[@]}; do
   echo "updating $i"

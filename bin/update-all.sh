5a96f63e3bc02272c9803ab70758cbdf529a55b0
Emerson Ford <emersontford@gmail.com>
2020-05-19 22:39:42 -0600

    clean up bin for dotbot

--- bin/update-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-hosts=(home fm-test miyako notchpeak.chpc.utah.edu cade hacktheu-main hacktheu-bigboi)
-
-for i in ${hosts[@]}; do
-  echo "updating $i"
-  ssh -o ConnectTimeout=5 $i 'bash -s' < ~/.dotfiles/bin/dotfile-install
-done

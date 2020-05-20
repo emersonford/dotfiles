7f1ab07fb20e848e9c53d3a134a2ff6a710ee5d3
Emerson Ford <emersontford@gmail.com>
2020-05-20 15:26:02 -0600

    Fix zsh tab completion colors

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,11 +7,11 @@ export EDITOR=$VISUAL
 export TERM='xterm-256color'
 
 # LS Colors
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-	export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
-else
+if ! ls --color > /dev/null 2>&1; then # GNU `ls`
     export LSCOLORS='exfxcxdxbxegedabagacad'
 fi
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
 # Locale
 export LANG='en_US.UTF-8'

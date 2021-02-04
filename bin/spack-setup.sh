e55c5e4aadf9df927c325be6bab01881a0a70c2d
Emerson Ford <emersontford@gmail.com>
2021-02-04 11:54:42 -0700

    Add git to spack setup

--- bin/spack-setup.sh
+++ bin/spack-setup.sh
@@ -10,8 +10,10 @@ spack/bin/spack external find
 spack install tmux@3.1b
 spack install vim@8.2.1201
 spack install zsh@5.8
+spack install git@2.29.0
 
 ln -sf "$(spack location --install-dir tmux@3.1b)/bin/tmux" ~/bin/tmux
 ln -sf "$(spack location --install-dir vim@8.2.1201)/bin/vim" ~/bin/vim
 ln -sf "$(spack location --install-dir vim@8.2.1201)/bin/vimdiff" ~/bin/vimdiff
 ln -sf "$(spack location --install-dir zsh@5.8)/bin/zsh" ~/bin/zsh
+ln -sf "$(spack location --install-dir git@2.29.0)/bin/git" ~/bin/git

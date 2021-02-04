8c9fc49ad72c83798353e63dd36d27e00e634842
Emerson Ford <emersontford@gmail.com>
2021-02-04 11:49:34 -0700

    Add spack setup script and point bash/bashrc to use it

--- bin/install-vim8-on-centos.sh
+++
@@ -1,8 +0,0 @@
-yum -y groupinstall "Development Tools"
-yum -y install ncurses-devel git-core
-
-git clone https://github.com/vim/vim && cd vim
-
-./configure --prefix=/usr --with-features=huge --enable-multibyte --with-python-config-dir=/usr/lib/python2.7/config --enable-pythoninterp=yes
-
-make && sudo make install

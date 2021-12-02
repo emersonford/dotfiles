d719d05218932a40537dd0d2ff3a8c72862eee67
Emerson Ford <emersonford@fb.com>
2021-12-02 14:05:06 -0800

    update plugin keybinds

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -74,7 +74,8 @@ return require('packer').startup(function(use)
       vim.api.nvim_set_keymap('n', 'gn', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', 'gp', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', 'gq', ':BufferLinePickClose<CR>', { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('n', 'gb', ':BufferLinePick<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'gh', ':BufferLinePick<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'gb', ':b#<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', 'g]', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', 'g[', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
     end
@@ -86,6 +87,14 @@ return require('packer').startup(function(use)
       require('lualine').setup{
         options = {
           theme = "onedark"
+        },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1
+            }
+          }
         }
       }
     end
@@ -151,7 +160,6 @@ return require('packer').startup(function(use)
           ['<C-e>'] = cmp.mapping.close(),
           ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
           },
           ['<Tab>'] = function(fallback)
             if cmp.visible() then
@@ -202,6 +210,78 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', 
     requires = { 'nvim-lua/plenary.nvim' },
     config = function()
+      -- Allow multi select https://github.com/nvim-telescope/telescope.nvim/issues/1048
+      local actions = require('telescope.actions')
+      local action_state = require('telescope.actions.state')
+      local custom_actions = {}
+
+      function custom_actions._multiopen(prompt_bufnr, open_cmd)
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        local num_selections = #picker:get_multi_selection()
+        if num_selections > 1 then
+          local cwd = picker.cwd
+          if cwd == nil then
+            cwd = ""
+          else
+            cwd = string.format("%s/", cwd)
+          end
+          vim.cmd("bw!") -- wipe the prompt buffer
+          for _, entry in ipairs(picker:get_multi_selection()) do
+            vim.cmd(string.format("%s %s%s", open_cmd, cwd, entry.value))
+          end
+          vim.cmd('stopinsert')
+        else
+          if open_cmd == "vsplit" then
+            actions.file_vsplit(prompt_bufnr)
+          elseif open_cmd == "split" then
+            actions.file_split(prompt_bufnr)
+          elseif open_cmd == "tabe" then
+            actions.file_tab(prompt_bufnr)
+          else
+            actions.select_default(prompt_bufnr)
+          end
+        end
+      end
+      function custom_actions.multi_selection_open_vsplit(prompt_bufnr)
+        custom_actions._multiopen(prompt_bufnr, "vsplit")
+      end
+      function custom_actions.multi_selection_open_split(prompt_bufnr)
+        custom_actions._multiopen(prompt_bufnr, "split")
+      end
+      function custom_actions.multi_selection_open_tab(prompt_bufnr)
+        custom_actions._multiopen(prompt_bufnr, "tabe")
+      end
+      function custom_actions.multi_selection_open(prompt_bufnr)
+        custom_actions._multiopen(prompt_bufnr, "edit")
+      end
+
+      require('telescope').setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+              ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+              ["<cr>"] = custom_actions.multi_selection_open,
+              ["<c-v>"] = custom_actions.multi_selection_open_vsplit,
+              ["<c-s>"] = custom_actions.multi_selection_open_split,
+              ["<c-t>"] = custom_actions.multi_selection_open_tab,
+            },
+            n = {
+              ["<esc>"] = actions.close,
+              ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+              ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+              ["<cr>"] = custom_actions.multi_selection_open,
+              ["<c-v>"] = custom_actions.multi_selection_open_vsplit,
+              ["<c-s>"] = custom_actions.multi_selection_open_split,
+              ["<c-t>"] = custom_actions.multi_selection_open_tab,
+            }
+          },
+        }
+      })
+
       vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
       vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })

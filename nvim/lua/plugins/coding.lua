5467f01bd4df30e1daed84f3c9e6165d9295ef01
Emerson Ford <emersonford@fb.com>
2025-06-09 12:58:47 -0700

    go back to enter preset?

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -2,9 +2,6 @@ return {
   {
     "saghen/blink.cmp",
     opts = {
-      keymap = {
-        preset = "super-tab",
-      },
       completion = {
         ghost_text = {
           enabled = false,
